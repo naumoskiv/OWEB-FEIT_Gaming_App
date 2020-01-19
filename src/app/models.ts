038422e21961b3fe0220abba6b776c676812f464
naumoskiv <58512274+naumoskiv@users.noreply.github.com>
2020-01-19 01:30:51 +0100

    dodadeni komentari

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,12 +1,17 @@
 class Game {
     id: number;
     photoUrl: string;
-}
-
-class GameDetails {
-    id: number;
     rating: number;
     name: string;
-    photoUrl: string;
+    logoUrl: string;
     description: string;
+    isVoted: boolean;
+}
+
+class GameComment {
+    id: number;
+    fullName: string;
+    accountName: string;
+    photoUrl: string;
+    comment: string;
 }
